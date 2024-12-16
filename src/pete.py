6c034a0f7e13bf170e5e230a9f54087d5f04b6ed
Siriwardanage Don Supun Madusanka <siriwardanage-don-supun.madusanka.5747@student.uu.se>
2024-12-16 20:16:52 +0100

    emotion detected with camera feed was added to pete

--- src/pete.py
+++ src/pete.py
@@ -1,10 +1,13 @@
 from furhat_remote_api import FurhatRemoteAPI
 import google.generativeai as genai
+from DetectFace import MyDetectFace
 
-genai.configure(api_key="AIzaSyDW9WPa7Oe2-S_SOdi4VzajU9M1YVKzBTs")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 chat = model.start_chat()
 llm_r = chat.send_message("what is ML?")
+
+mdf = MyDetectFace()
 
 furhat = FurhatRemoteAPI("localhost")
 voices = furhat.get_voices()
@@ -16,7 +19,7 @@ furhat.set_face(character="Isabel", mask="adult")
 furhat.set_voice(name='Joanna')
 
 # Have Furhat greet the user
-furhat.say(text="hi", blocking=False)
+furhat.say(text="hi", blocking=True)
 
 
 while(True):
@@ -25,7 +28,8 @@ while(True):
 
     # Check if listening was successful
     if response.success and response.message:
-        print("User said:", response.message)
-        user_r = response.message
+        emo, face = mdf.getUserEmotion()
+        user_r = f'User looks {emo} and user ask {response.message}, what would you say?' 
+        print("User req:", user_r)
         llm_r = chat.send_message(user_r)
         furhat.say(text=llm_r.text, blocking=True)
