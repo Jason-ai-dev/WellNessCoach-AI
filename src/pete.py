6551ef9139b79437e4102dcb3bac8dbfd7b4a05e
Siriwardanage Don Supun Madusanka <siriwardanage-don-supun.madusanka.5747@student.uu.se>
2024-12-16 20:34:09 +0100

    expert prompt was added.

--- src/pete.py
+++ src/pete.py
@@ -5,7 +5,15 @@ from DetectFace import MyDetectFace
 genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 chat = model.start_chat()
-llm_r = chat.send_message("what is ML?")
+
+expert_prompt = (
+"You are an experienced mental health counselor with expertise in helping individuals with emotional issues"
+"Your goal is to provide thoughtful, empathetic, and professional advice to users"
+"You are supposed to good at dealing with mental health challenges"
+"You should base your responses on psychological principles and practical solutions. "
+"Read and understand the users' questions, write responses at high levels of empathic understanding. "
+"Limit each response to a minimum of 100 words and a maximum of 200 words. ")
+llm_r = chat.send_message(expert_prompt)
 
 mdf = MyDetectFace()
 
@@ -29,7 +37,7 @@ while(True):
     # Check if listening was successful
     if response.success and response.message:
         emo, face = mdf.getUserEmotion()
-        user_r = f'User looks {emo} and user ask {response.message}, what would you say?' 
+        user_r = f'User looks {emo} and user says {response.message}' 
         print("User req:", user_r)
         llm_r = chat.send_message(user_r)
         furhat.say(text=llm_r.text, blocking=True)
